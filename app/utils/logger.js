a5ede6f051101a15e239a7d8bf312cc20066f688
Michael Rosata <mrosata1984@gmail.com>
2017-01-29 14:25:05 -0500

    fix(logger, bootstrap)

--- app/utils/logger.js
+++ app/utils/logger.js
@@ -1,4 +1,4 @@
-const _console = console && typeof console === "object" ? _console : {};
+const _console = console && typeof console === "object" ? console : {};
 export const log = 'log' in _console ? console.log.bind(console) : () => {};
 export const info = 'info' in _console ? console.info.bind(console) : () => {};
 export const error = 'error' in _console ? console.error.bind(console) : () => {};
