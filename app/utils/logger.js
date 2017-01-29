1a10e45dfaadaa2c8bca0bd374a5cee23e9b8433
Michael Rosata <mrosata1984@gmail.com>
2017-01-29 00:20:30 -0500

    update(console logging)

--- app/utils/logger.js
+++ app/utils/logger.js
@@ -1,3 +1,5 @@
-export const log = console.log.bind(console);
-export const info = console.info.bind(console);
-export const error = console.error.bind(console);
+const _console = console && typeof console === "object" ? _console : {};
+export const log = 'log' in _console ? console.log.bind(console) : () => {};
+export const info = 'info' in _console ? console.info.bind(console) : () => {};
+export const error = 'error' in _console ? console.error.bind(console) : () => {};
+export const debug = 'debug' in _console ? console.debug.bind(console) : () => {};
