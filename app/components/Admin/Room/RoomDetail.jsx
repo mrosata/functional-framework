dad8db8cf05acaa66683c94947ca80d9236fb915
Mark Fuller <mark.fuller@tylertech.com>
2017-02-18 12:03:40 -0500

    Inital form layout.

--- app/components/Admin/Room/RoomDetail.jsx
+++ app/components/Admin/Room/RoomDetail.jsx
@@ -2,11 +2,13 @@
 import dom from '../../../utils/dom';
 import Room from './room-class.js'
 
+const tempRoom = new Room();
+
 function addTestRoom() {
     console.log('addTestRoom');
 
     return new Promise((resolve, reject) => {
-        let room = new Room(3, 'Test Room', '3rd floor', true);
+        let room = new Room(115, 'Large Meeting Room', '1st floor', true);
         console.log(room);
 
         //validate
@@ -15,40 +17,56 @@ function addTestRoom() {
             throw "Before Saving Room To Firebase Ensure It Has Proper Info"
         }
 
-        //
-        Room.saveToFirebase(room);
+        //save to firebase
+        //Room.saveToFirebase(room);
+
+        //need to dispatch?
 
     })
+}
 
-    // Get the events from firebase
-    //   return new Promise((resolve, reject) => {
-    //     return DemoEvent.ref().on('value', (snapshot) => {
-    //       // map the events from firebase snapshot into DemoEvent class instances
-    //       try {
-    //         let events = []
-    //         snapshot.forEach(snap => {
-    //           events.push(DemoEvent.fromFirebaseSnapshot(snap))
-    //         })
 
-    //         return resolve(events)
-    //       }
-    //       catch(e) {return reject(e)}
-
-    //     })
-    //   })
+function handleNameChange(event) {
+    tempRoom.name = event.target.value;
+    console.log(tempRoom);
 }
 
 
 export default () => {
 
     return (
-        <div>
-            <span className="btn btn-success"
-                onclick={
-                    () => addTestRoom()}>Add Room</span>
-            <form>
-                <input></input>
-            </form>
+        <div class="container">
+            <div>
+                <form>
+                    <div class="form-group row">
+                        <label for="room_number" class="col-2 col-form-label">Room #</label>
+                        <div class="col-10">
+                            <input class="form-control" type="text" value="" placeholder="Room Number" id="room_number"></input>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="room_name" class="col-2 col-form-label">Room Name</label>
+                        <div class="col-10">
+                            <input class="form-control" type="text" required value="" placeholder="Room Name" onchange={handleNameChange} id="room_name"></input>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="room_desc" class="col-2 col-form-label">Description</label>
+                        <div class="col-10">
+                            <input class="form-control" type="text" required value="" placeholder="Description" onchange={handleNameChange} id="room_desc"></input>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="room_active" class="col-2 col-form-label">Active</label>
+                        <div class="col-10">
+                            <input class="form-control" type="checkbox" value="" placeholder="Active" onchange={handleNameChange} id="room_active"></input>
+                        </div>
+                    </div>
+                </form>
+                <span className="btn btn-success"
+                    onclick={
+                        () => addTestRoom()}>Add Room</span>
+            </div>
         </div>
     );
 }
