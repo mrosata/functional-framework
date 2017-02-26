c132ca418083b785fa119cc185f84ca3a7181587
Mark Grover <markgrover@Marks-MacBook-Pro.local>
2017-02-25 22:04:56 -0500

    added google calendar

--- app/components/Calendar.jsx
+++ app/components/Calendar.jsx
@@ -23,6 +23,11 @@ jQuery(document).ready(function($) {
   $('#calendar').fullCalendar({
     header: { right: 'month,agendaWeek,agendaDay' }, // buttons for switching between views
 
+    googleCalendarApiKey: 'AIzaSyBKBzjJk06ARQsT8l1GuQSDfyppuIJEBjg',
+    eventSources: [{
+      googleCalendarId: 'ao2er574u284sfcoig545dp4vc@group.calendar.google.com',
+      className: 'gcal-event'
+    }],
     dayClick: function() {
       log('a day has been clicked!');
     }
